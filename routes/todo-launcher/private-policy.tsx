fe774293436e89053d751976a969d0ac00349e28
hernadno pinzon <hernadnonunez13@gmail.com>
2023-04-30 16:37:26 -0500

    First commit

---
+++ routes/todo-launcher/private-policy.tsx
@@ -0,0 +1,12 @@
+import { Head } from "$fresh/runtime.ts";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Privacy policy</title>
+      </Head>
+      <h1>Privacy policy</h1>
+    </>
+  );
+}
